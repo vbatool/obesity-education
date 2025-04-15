13ea28740925cb5f2185d9f43d6b410ae4ed97f7
oizokun <techupnorth204@gmail.com>
2025-04-15 14:28:48 -0400

    Responsive navbar

--- src/components/navigationBar/NavigationBar.js
+++ src/components/navigationBar/NavigationBar.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./NavigationBar.css";
 import DartmouthHealthLogo from "../../images/NavigationBarImages/DartmouthHealthLogo.png";
+
 function NavigationBar() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const toggleMenu = () => {
+    setIsClicked(!isClicked);
+  };
+
   return (
     <nav className="NavigationBar">
       <div className="NavigationBarLeft">
-        <img
-          className="DartmouthHealthLogo"
-          src={DartmouthHealthLogo}
-          alt="Dartmouth Health Logo"
-        ></img>
-        <li>
-          <Link className="NavigationBarLinkItem">Dartmouth Health</Link>
-        </li>
+        <div className="LogoContainer">
+          <img
+            className="DartmouthHealthLogo"
+            src={DartmouthHealthLogo}
+            alt="Dartmouth Health Logo"
+          ></img>
+          <li>
+            <Link className="NavigationBarLinkItem">Dartmouth Health</Link>
+          </li>
+        </div>
+
+        <div className="NavigationBarMenuIcon" onClick={toggleMenu}>
+          ☰
+        </div>
       </div>
-      <div className="NavigationBarRight">
+
+      <div className={`NavigationBarRight ${isClicked ? "Clicked" : ""}`}>
         <ul className="NavigationBarLinkList">
           <li>
             <Link className="NavigationBarLinkItem">Health Topics</Link>
